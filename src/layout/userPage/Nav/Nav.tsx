55a18777b06e169dbe23756b44eefe9a6b2bbf5b
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-11-05 11:20:23 -0700

    real time mutations

--- src/layout/userPage/Nav/Nav.tsx
+++ src/layout/userPage/Nav/Nav.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom"
+import { BsBook } from 'react-icons/bs';
+import { ImBook } from 'react-icons/im';
 
 const Nav = () => {
     return (
         <div className="user--links">
-            <NavLink to="/user/overview" className=" navLinks" >Overview</NavLink>
-            <NavLink to="/user/repos" className=" navLinks">Repositories</NavLink>
+            <NavLink to="/user/overview" className=" navLinks" ><BsBook /> Overview</NavLink>
+            <NavLink to="/user/repos" className=" navLinks"><ImBook /> Repositories</NavLink>
         </div>
     )
 }
--- src/layout/userPage/userDetail/UserDetail.tsx
+++ src/layout/userPage/userDetail/UserDetail.tsx
@@ -1,10 +1,9 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import moment from 'moment';
 import { useContext } from 'react';
 import Error from '../../../components/Error/Error';
 import Loading from '../../../components/Loading/Loading';
 import { GlobalContext } from '../../../context/AppContext';
-import './style.css'
 
 
 const UserDetail = () => {
@@ -32,26 +31,27 @@ const UserDetail = () => {
 
   if (loading) return <Loading />;
   if (error) return <Error error={error} />;
+  const { user } = data;
 
   return (
     <div className="user-details">
       <img
         className="user--img"
-        src={data?.user?.avatarUrl}
+        src={user?.avatarUrl}
       />
 
       <div className="user__text--info">
-        <h2>{data?.user?.name}</h2>
-        <h3>{data?.user?.bio}</h3>
+        <h2 className="user__text--primary">{user?.name}</h2>
+        <h3 className="user__text--secondary">{user?.bio}</h3>
 
       </div>
       <div className="user__action--info">
-        <h4>{data?.user?.followers?.totalCount} followers</h4>
-        <h4>{data?.user?.following?.totalCount} following</h4>
+        <h4 className="user__text--tertiary">{user?.followers.totalCount} followers</h4>
+        <h4 className="user__text--tertiary">{user?.following?.totalCount} following</h4>
       </div>
       <h3 className="user__text--date">
         Created At:
-        <span> {moment(data?.user?.createdAt).format('LL')}
+        <span> {moment(user?.createdAt).format('LL')}
         </span></h3>
     </div>
   )
